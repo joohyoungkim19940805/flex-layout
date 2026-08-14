43c34ed18f426a29cdfedf7759abd95544193144
joohyoungkim19940805 <oozu1994@gmail.com>
2026-08-14 10:14:11 +0900

    원복

--- src/next/server/index.ts
+++ src/next/server/index.ts
@@ -1,11 +1,16 @@
-export { createFlexLayoutNextSplitScreen } from "./FlexLayoutNextSplitScreen";
-export type { CreateFlexLayoutNextSplitScreenOptions } from "./FlexLayoutNextSplitScreen";
-
-export { resolvePageFromRegistry } from "./pageRegistry";
+export {
+	createFlexLayoutNextSplitScreen,
+	FlexLayoutNextSplitScreen,
+} from "./FlexLayoutNextSplitScreen";
+export type {
+	CreateFlexLayoutNextSplitScreenOptions,
+	FlexLayoutNextSplitScreenProps,
+} from "./FlexLayoutNextSplitScreen";
+export {
+	resolvePageFromRegistry,
+} from "./pageRegistry";
 export type { ResolveFlexLayoutNextPageOptions } from "./pageRegistry";
-
 export { renderResolvedPage } from "./renderResolvedPage";
-
 export type {
 	FlexLayoutNextCookieOptions,
 	FlexLayoutNextPageImporter,
